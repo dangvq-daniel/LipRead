e4bfb6cfd296c9c1ab356bce47a813135e10c606
dangvq-daniel <57842685+dangvq-daniel@users.noreply.github.com>
2023-05-31 13:25:27 -0400

    Update streamlitapp.py

--- app/streamlitapp.py
+++ app/streamlitapp.py
@@ -21,8 +21,6 @@ with st.sidebar:
     st.info('This application is originally developed from the LipNet deep learning model.')
 
 st.title('Lip Net Full Stack App')
-st.text(os.listdir())
-os.chdir('./app')
 
 options = os.listdir(os.path.join('..', 'data', 's1'))
 selected_video = st.selectbox('Choose Video', options)
