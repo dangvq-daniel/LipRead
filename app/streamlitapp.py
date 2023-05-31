a7a98015c5d59f50842dde39659d00a57cb9d686
dangvq-daniel <57842685+dangvq-daniel@users.noreply.github.com>
2023-05-31 00:17:17 -0400

    Update streamlitapp.py

--- app/streamlitapp.py
+++ app/streamlitapp.py
@@ -19,6 +19,7 @@ with st.sidebar:
     st.info('This application is originally developed from the LipNet deep learning model.')
 
 st.title('Lip Net Full Stack App')
+st.text(os.cwd())
 new_dir = '/app'
 os.chdir(new_dir)
 
