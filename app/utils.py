67e325f85d24817a08d30bba493a908b194de934
dangvq-daniel <57842685+dangvq-daniel@users.noreply.github.com>
2023-05-31 12:56:49 -0400

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from typing import List
 import cv2
 import os
-import streamlit as st
+import ffmpeg
 
 vocab = [x for x in "abcdefghijklmnopqrstuvwxyz'?!123456789 "]
 char_to_num = tf.keras.layers.StringLookup(vocabulary=vocab, oov_token="")
