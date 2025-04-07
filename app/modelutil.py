2e884ce99c8ebb5903c9a363158548827e814c5f
dangvq-daniel <57842685+dangvq-daniel@users.noreply.github.com>
2025-04-07 16:20:10 -0400

    Update modelutil.py

--- app/modelutil.py
+++ app/modelutil.py
@@ -28,6 +28,6 @@ def load_model() -> Sequential:
 
     model.add(Dense(41, kernel_initializer='he_normal', activation='softmax'))
 
-    model.load_weights(os.path.join('..','models','checkpoint'))
+    model.load_weights(os.path.join('.','models','checkpoint'))
 
     return model
